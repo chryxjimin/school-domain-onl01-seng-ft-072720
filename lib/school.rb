2e19ab471d03694b8cff87b2f3357572b11af2d8
Christie Yoon <cleex1999@gmail.com>
2020-08-09 01:41:17 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,16 +11,10 @@ class School
     end
 
     def add_student(name, grade)
-        roster(grade) = name
+        
     end
 
-    def grade=(grade)
-      @grade = grade
-    end
 
-    def grade
-      @grade
-    end
 binding.pry
 
 end
