6326cce3350aaa28241ace38adb1e01bb44e7d08
Christie Yoon <cleex1999@gmail.com>
2020-08-09 01:39:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,32 +1,26 @@
-require 'pry'
-
+require "pry"
 class School
 
-  def initialize(school_name)
-    @school_name = school_name
-    @roster = {}
-  end
-
-  def roster
-    @roster
-  end
-
-  def add_student(name, grade)
-    @student_name = name
-    @grade = grade
-    if @roster.include?(grade) == false
-      @roster[grade] = []
+    def initialize(school_name)
+      @school_name = school_name
+      @roster = {}
     end
-    @roster[grade] << name
-  end
 
-  def grade(number)
-    @roster[number]
-  end
-
-  def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
+    def roster
+      @roster
     end
-  end
+
+    def add_student(name, grade)
+        roster(grade) = name
+    end
+
+    def grade=(grade)
+      @grade = grade
+    end
+
+    def grade
+      @grade
+    end
+binding.pry
+
 end
