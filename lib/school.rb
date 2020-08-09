b19e136e3f5532b976229f75a4cc40733bd89c02
Christie Yoon <cleex1999@gmail.com>
2020-08-09 01:26:08 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+require "pry"
+class School
+
+    def initialize(name)
+      @name = name
+      @roster = {}
+    end
+
+    def roster
+      @roster
+    end
+
+    def add_student(name, grade)
+        roster(grade) = name
+    end
+
+    def grade=(grade)
+      @grade = grade
+    end
+
+    def grade
+      @grade
+    end
+binding.pry
+
+end
